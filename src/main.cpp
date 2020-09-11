e938c03cba0b75aa530250f268541214a6d4731b
Eugene Mwangi <mwangieugen80@gmail.com>
2020-09-11 16:47:08 +0300

    Adds a config.h file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
+#include "mhz19_uart.h"
+
+#if CO2_SENSOR_ENABLED
+#endif // CO2_SENSOR_ENABLED
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  MHZ19_uart.begin();
 }
 
 void loop() {
